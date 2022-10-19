9a494310b42b519586a1c42948c32d97717cc1f9
pavel <p.topilin@inbox.ru>
2022-10-19 16:17:57 +0300

    Заверстал страничку чтения статьи

--- resources/views/read-post.blade.php
+++ resources/views/read-post.blade.php
@@ -6,7 +6,37 @@
 
 @section('content')
 
-<p>{{ $post->title }}</p>
+<div class="p-4 mt-4 container border rounded overflow-hidden shadow-sm">
+    <div class="text-center">
+        <h3 class="fs-1">{{ $post->title }}</h3>
+    </div>
+    @if(!empty($post->image))
+    <div>
+        <img src="{{ url('public/Image/'.$post->image) }}" class="rounded mx-auto img-thumbnail mh-50 mw-50 h-50 w-50" alt="Responsive image">
+    </div>
+    @endif
+    <div class="text-start mt-3">
+        <p class="fs-3">{{ $post->content }}</p>
+    </div>
+    <div class="text-start mt-3">
+        <div class="text-start mt-3 text-muted">
+            @if(!empty(Auth::user()->id) and (Auth::user()->id == $post->author->id))
+            <p class="fs-5">Author: <a href="{{ route('user-profile') }}" >{{ $post->author->name }}</a></p>
+            @else
+            <p class="fs-5">Author: <a href="{{ route('another-user-profile', $post->author->id) }}" >{{ $post->author->name }}</a></p>
+            @endif
+        </div>
+        <div class="text-start mt-3 row">
+            <p class="fs-5 col-4 text-muted">Created at: {{ $post->created_at }}</p>
+            <p class="fs-5 col-4 text-muted">Updated at: {{ $post->updated }}</p>
+            @if(!empty(Auth::user()->id) and (Auth::user()->id == $post->author->id))
+            <p class="fs-5 col-4 text-end text-primary"><a href="{{ route('edit-post', $post->id) }}">Edit this post</a></p>
+            @else
+            <p class="fs-5 col-4 text-end text-primary">Like this post</p>
+            @endif
+        </div>
+    </div>
+</div>
 
 @endsection
 
